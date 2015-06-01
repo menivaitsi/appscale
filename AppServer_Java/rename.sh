a33875c8390f409a6b78bf742781efc06ceaf9e5
Casey O'Neill <j.casey.oneill@gmail.com>
2015-06-01 08:40:20 -0600

    created script to rename 1.8.1 files back to 1.8.0 after build

---
+++ AppServer_Java/rename.sh
@@ -0,0 +1,4 @@
+mv appengine-java-sdk-repacked/lib/user/appengine-api-labs-1.8.1.jar appengine-java-sdk-repacked/lib/user/appengine-api-labs-1.8.0.jar
+mv appengine-java-sdk-repacked/lib/user/appengine-api-1.0-sdk-1.8.1.jar appengine-java-sdk-repacked/lib/user/appengine-api-1.0-sdk-1.8.0.jar
+mv appengine-java-sdk-repacked/lib/user/appengine-jsr107cache-1.8.1.jar appengine-java-sdk-repacked/lib/user/appengine-jsr107cache-1.8.0.jar
+mv appengine-java-sdk-repacked/lib/opt/user/jsr107/v1/appengine-jsr107cache-1.8.1.jar appengine-java-sdk-repacked/lib/opt/user/jsr107/v1/appengine-jsr107cache-1.8.0.jar
